858cd0ded85728bc475e6039679fd9770dcfc4e2
indiofearth <indi.caburian@outlook.com>
2022-10-05 13:07:02 +0100

    Restructured into package

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Setting FLASK_ENV and Config
+# USE: set FLASK_ENV=development or production BEFORE "flask run"
+if app.config["ENV"] == "production":
+    app.config.from_object("app.config.ProductionConfig")
+elif app.config["ENV"] == "development":
+    app.config.from_object("app.config.DevelopmentConfig")
+
+
+# from app import
